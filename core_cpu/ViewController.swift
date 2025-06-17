6467f61eab52d6846d2f7a030e746d5b72a7a145
sharon <sharon.kosman@yandex.ru>
2025-06-17 17:58:43 +0300

    nothing to say

---
+++ core_cpu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  core_cpu
+//
+//  Created by Шарон on 17.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
