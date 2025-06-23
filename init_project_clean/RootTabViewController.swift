434c17c6425c35ff5ae49388aa926d04311a8e2f
sharon <sharon.kosman@yandex.ru>
2025-06-23 16:16:04 +0300

    RootTabViewController + TasksViewController

--- init_project_clean/RootTabViewController.swift
+++ init_project_clean/RootTabViewController.swift
@@ -4,5 +4,25 @@
 //
 //  Created by Шарон on 23.06.2025.
 //
-
 import Foundation
+import UIKit
+
+
+final class RootTabViewController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTabs()
+    }
+
+    private func setupTabs() {
+        
+        let tasksVC = TasksViewController()
+        tasksVC.tabBarItem = UITabBarItem(
+            title: "Задачи",
+            image: UIImage(systemName: "list.bullet"),
+            tag: 0
+        )
+
+        viewControllers = [tasksVC]
+    }
+}
--- init_project_clean/TasksViewController.swift
+++ init_project_clean/TasksViewController.swift
@@ -4,5 +4,20 @@
 //
 //  Created by Шарон on 23.06.2025.
 //
-
 import Foundation
+import UIKit
+
+final class TasksViewController: UIViewController {
+
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+
+    // MARK: - Setup
+    private func setupView() {
+        view.backgroundColor = .systemBackground // Автоматически под темную темку
+        title = "Задачи" // Заголовок для NavigationBar
+    }
+}
