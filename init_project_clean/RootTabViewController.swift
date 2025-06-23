9d2db1453be23f2e3c8df9a5bc769a1213aa3acf
sharon <sharon.kosman@yandex.ru>
2025-06-23 16:15:10 +0300

    added TasksViewController + RootTabViewController "задачи"

---
+++ init_project_clean/RootTabViewController.swift
@@ -0,0 +1,8 @@
+//
+//  RootTabViewController.swift
+//  init_project_clean
+//
+//  Created by Шарон on 23.06.2025.
+//
+
+import Foundation
---
+++ init_project_clean/TasksViewController.swift
@@ -0,0 +1,8 @@
+//
+//  TasksViewController.swift
+//  init_project_clean
+//
+//  Created by Шарон on 23.06.2025.
+//
+
+import Foundation
