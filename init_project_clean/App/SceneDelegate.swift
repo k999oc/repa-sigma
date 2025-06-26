59ce9d041f2bf782089bc1ca546fc428390c555c
sharon <sharon.kosman@yandex.ru>
2025-06-26 18:51:16 +0300

    deleted the lifecycles

--- init_project_clean/App/SceneDelegate.swift
+++ init_project_clean/App/SceneDelegate.swift
@@ -5,41 +5,20 @@
 //  Created by Шарон on 01.06.2025.
 //
 import UIKit
-
-// MARK: - ФУНКЦИИ ЖИЗНЕННОГО ЦИКЛА ПРИЛОЖЕНИЯ
-func sceneDidDisconnect(_ scene: UIScene) {
-    // Ресурсы можно освободить здесь
-}
-
-func sceneDidBecomeActive(_ scene: UIScene) {
-    // Возобновление задач при активации
-}
-
-func sceneWillResignActive(_ scene: UIScene) {
-    // Пауза при переходе в неактивное состояние
-}
-
-func sceneWillEnterForeground(_ scene: UIScene) {
-    // Возврат из фона
-}
-
-func sceneDidEnterBackground(_ scene: UIScene) {
-    // Сохранение данных при уходе в фон
-}
-
+import Foundation
 
 // MARK: - "ЗАДАЧИ"
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
+    
     func scene(_ scene: UIScene,
                willConnectTo session: UISceneSession,
                options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-
+        
         let window = UIWindow(windowScene: windowScene)
-
+        
         let tasksVC = TasksViewController()
         let tasksNavVC = UINavigationController(rootViewController: tasksVC)
         tasksNavVC.tabBarItem = UITabBarItem(title: "Задачи",
@@ -47,7 +26,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                                              selectedImage: UIImage(systemName: "square.stack.fill"))
         let tabBarController = UITabBarController()
         tabBarController.viewControllers = [tasksNavVC]
-
+        
         window.rootViewController = tabBarController
         self.window = window
         window.makeKeyAndVisible()
